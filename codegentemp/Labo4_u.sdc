ace444bac9b0688de21fb975bb3a82a2f129940f
Boyczum <ariane.boyczum-auger@polymtl.ca>
2021-03-10 15:45:07 -0500

    part 1 completed

--- codegentemp/Labo4_u.sdc
+++ codegentemp/Labo4_u.sdc
@@ -1,3 +1,3 @@
 # Component constraints for C:\Users\Ariane\Documents\PSoC Creator\Laboratoire4\Labo4.cydsn\TopDesign\TopDesign.cysch
 # Project: C:\Users\Ariane\Documents\PSoC Creator\Laboratoire4\Labo4.cydsn\Labo4.cyprj
-# Date: Wed, 10 Mar 2021 19:53:31 GMT
+# Date: Wed, 10 Mar 2021 20:24:42 GMT
--- main_cm4.c
+++ main_cm4.c
@@ -16,16 +16,31 @@
 #include "params.h"
 #include "queue.h"
 
+
+void vLedTask(){
+    for(;;){
+    Cy_GPIO_Write(LED_PORT,LED_NUM,1);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    Cy_GPIO_Write(LED_PORT,LED_NUM,0);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
 int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    xTaskCreate(vLedTask,"BLINK LED",80,NULL,3,NULL);
+    
+    vTaskStartScheduler();
+    
     for(;;)
     {
         /* Place your application code here. */
     }
+    
+   
 }
 
 /* [] END OF FILE */
