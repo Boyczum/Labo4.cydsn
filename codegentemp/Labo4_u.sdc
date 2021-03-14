8e764d38ec5bc2ddde368791588d8f668d85c972
Boyczum <ariane.boyczum-auger@polymtl.ca>
2021-03-13 23:21:17 -0500

    part 2 completed

--- codegentemp/Labo4_u.sdc
+++ codegentemp/Labo4_u.sdc
@@ -1,3 +1,3 @@
 # Component constraints for C:\Users\Ariane\Documents\PSoC Creator\Laboratoire4\Labo4.cydsn\TopDesign\TopDesign.cysch
 # Project: C:\Users\Ariane\Documents\PSoC Creator\Laboratoire4\Labo4.cydsn\Labo4.cyprj
-# Date: Wed, 10 Mar 2021 20:24:42 GMT
+# Date: Sun, 14 Mar 2021 04:17:32 GMT
--- main_cm4.c
+++ main_cm4.c
@@ -17,22 +17,58 @@
 #include "queue.h"
 
 
+SemaphoreHandle_t bouton_semph = NULL;
+
 void vLedTask(){
     for(;;){
-    Cy_GPIO_Write(LED_PORT,LED_NUM,1);
-    vTaskDelay(pdMS_TO_TICKS(500));
-    Cy_GPIO_Write(LED_PORT,LED_NUM,0);
-    vTaskDelay(pdMS_TO_TICKS(500));
+        Cy_GPIO_Write(LED_PORT,LED_NUM,1);
+        vTaskDelay(pdMS_TO_TICKS(500));
+        Cy_GPIO_Write(LED_PORT,LED_NUM,0);
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
 
+void isr_bouton(){
+    
+    xSemaphoreGiveFromISR(bouton_semph,NULL);
+    Cy_GPIO_ClearInterrupt(Bouton_0_PORT, Bouton_0_NUM);
+    NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);
+}
+
+void bouton_task(){
+
+    for(;;){
+        
+        xSemaphoreTake(bouton_semph,portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(20));
+            
+        if(Cy_GPIO_Read(Bouton_0_PORT,Bouton_0_NUM)==0){
+            UART_1_PutString("Boutonappuye \n\r");
+        }
+        else if(Cy_GPIO_Read(Bouton_0_PORT,Bouton_0_NUM)==1){
+            UART_1_PutString("Boutonrelache \n\r");
+        }    
+        
+        bouton_semph = xSemaphoreCreateBinary();
+    }
+}
+
+
 int main(void)
 {
+    bouton_semph = xSemaphoreCreateBinary();
     __enable_irq(); /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    xTaskCreate(vLedTask,"BLINK LED",80,NULL,3,NULL);
+    Cy_SysInt_Init(&Bouton_ISR_cfg, isr_bouton);
+    NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);
+    NVIC_EnableIRQ(Bouton_ISR_cfg.intrSrc);
     
+    UART_1_Start();
+    
+    xTaskCreate(vLedTask,"BLINK LED",80,NULL,3,NULL);
+    xTaskCreate(bouton_task,"BOUTON",80,NULL,3,NULL);
+
     vTaskStartScheduler();
     
     for(;;)
