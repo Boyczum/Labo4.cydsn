d0e10b72a3a8bfc1fd5850784d7d9b9e82d7d769
Boyczum <ariane.boyczum-auger@polymtl.ca>
2021-03-15 00:04:19 -0400

    part 3b bonus completed

--- main_cm4.c
+++ main_cm4.c
@@ -9,6 +9,9 @@
  *
  * ========================================
 */
+
+// CODE GITHUB: https://github.com/Boyczum/Labo4.cydsn.git
+
 #include "project.h"
 #include "FreeRTOS.h"
 #include "task.h"
@@ -16,9 +19,8 @@
 #include "params.h"
 #include "queue.h"
 
-
+QueueHandle_t print_queue;
 SemaphoreHandle_t bouton_semph = NULL;
-
 
 task_params_t task_A = {
     .delay = 1000,
@@ -38,7 +40,6 @@ void vLedTask(){
 }
 
 void isr_bouton(){
-    
     xSemaphoreGiveFromISR(bouton_semph,NULL);
     Cy_GPIO_ClearInterrupt(Bouton_0_PORT, Bouton_0_NUM);
     NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);
@@ -47,7 +48,6 @@ void isr_bouton(){
 void bouton_task(){
 
     for(;;){
-        
         xSemaphoreTake(bouton_semph,portMAX_DELAY);
         vTaskDelay(pdMS_TO_TICKS(20));
             
@@ -57,26 +57,35 @@ void bouton_task(){
         else if(Cy_GPIO_Read(Bouton_0_PORT,Bouton_0_NUM)==1){
             UART_1_PutString("Boutonrelache \n\r");
         }    
-        
         bouton_semph = xSemaphoreCreateBinary();
     }
 }
 
 void print_loop(void * params){
     task_params_t parametre = *(task_params_t *)params;
+    char* pointeur = &(parametre.message[0]);
+    
     for(;;){
         vTaskDelay(pdMS_TO_TICKS(parametre.delay));
-        UART_1_PutString(parametre.message);
+        xQueueSend(print_queue,&pointeur,portMAX_DELAY ); 
     }
-    
+}
+
+void print(){
+    char * message;
+    for(;;){
+        xQueueReceive(print_queue, &message, portMAX_DELAY);
+        UART_1_PutString(message);
+    }
 }
 
 int main(void)
 {
+        /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     bouton_semph = xSemaphoreCreateBinary();
+    print_queue = xQueueCreate(2,sizeof(char *));
+    
     __enable_irq(); /* Enable global interrupts. */
-
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     Cy_SysInt_Init(&Bouton_ISR_cfg, isr_bouton);
     NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);
     NVIC_EnableIRQ(Bouton_ISR_cfg.intrSrc);
@@ -85,7 +94,7 @@ int main(void)
     
     xTaskCreate(vLedTask,"BLINK LED",80,NULL,3,NULL);
     xTaskCreate(bouton_task,"BOUTON",80,NULL,3,NULL);
-    
+    xTaskCreate(print,"PRINT",80,NULL,3,NULL);
     xTaskCreate(print_loop,"task A",configMINIMAL_STACK_SIZE, (void *) &task_A, 1,NULL);
     xTaskCreate(print_loop,"task B", configMINIMAL_STACK_SIZE, (void *) &task_B,1,NULL);
     
@@ -95,8 +104,6 @@ int main(void)
     {
         /* Place your application code here. */
     }
-    
-   
 }
 
 /* [] END OF FILE */
